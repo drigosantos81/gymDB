33c6d76075d34f777c3cae6d21338ca2462f23d0
Rodrigo Emanuel <drigosantos@gmail.com>
2020-05-15 09:36:16 -0300

    Complemento na organização das pastas.

---
+++ src/app/controllers/instructors.js
@@ -0,0 +1,58 @@
+const { age, date, birthDay } = require('../../lib/utils');
+
+module.exports = {
+    // index
+    index(req, res) {
+        return res.render("instructors/index");
+    },
+
+    // Exibe a página create
+    create(req, res) {
+        return res.render("instructors/create");
+    },
+
+    // create - POST (Exibe a página para preencher com os dados)
+    post(req, res) {
+        const keys = Object.keys(req.body);
+
+        for (key of keys) {
+            if (req.body[key] == "") {
+                return res.send("Por favor, preencha todos os campos.");
+            }
+        }
+
+    let { avatar_url, name, birth, gender, services } = req.body;
+
+    return
+
+    },
+
+    // show (Exibe a página com os dados do registro)
+    show(req, res) {
+        return
+    
+    },
+
+    // edit (Exibe formulário com os dados disponivéis para alteração)
+    edit(req, res) {
+        return
+    },
+
+    // update - PUT (Comando de atualização)
+    put(req, res) {
+        const keys = Object.keys(req.body);
+
+        for (key of keys) {
+            if (req.body[key] == "") {
+                return res.send("Por favor, preencha todos os campos.");
+            }
+        }
+
+        return
+    },
+
+    // delete - DELETE (Comando de deletar)
+    delete(req, res) {
+        return
+    },
+}
