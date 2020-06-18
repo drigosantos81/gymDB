e3ea1c311ba07a2d1ce81e9960915e2202510907
Rodrigo Emanuel <drigosantos@gmail.com>
2020-06-18 18:07:28 -0300

    Implantado código de paginação no backend.

--- src/app/models/Instructor.js
+++ src/app/models/Instructor.js
@@ -105,5 +105,32 @@ module.exports = {
 
             return callback();
         })
+    },
+
+    paginate(params) {
+        const { filter, limit, offset , callback } = params;
+
+        let query = `
+            SELECT instructors.*, COUNT(members) as total_students FROM instructors
+            LEFT JOIN members ON (instructors.id = members.instructor_id)`
+
+        if (filter) {
+            query = `${query}
+            WHERE instructors.name ILIKE '%${filter};%'
+            OR instructors.services ILIKE '%${filter};%'`
+        }
+
+        query = `${query}
+            GROUP BY instructors.id LIMIT $1 OFFSET $2`
+
+        
+        db.query(query, [limit, offset], function(err, results) {
+            if (err) {
+                throw `Database error! ${err}`;
+            }
+
+            callback(results.rows);
+        });
     }
+
 }
