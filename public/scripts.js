d4e52ea30a48e5501b66f0e253c4cd6723d1f7b1
Rodrigo Emanuel <drigosantos@gmail.com>
2020-06-18 18:28:24 -0300

    script de organização da paginação.Iiniciado primeiros testes com conloe.log.

--- public/scripts.js
+++ public/scripts.js
@@ -6,3 +6,42 @@ for (item of menuItens) {
         item.classList.add("active");
     };
 };
+
+let totalPages = 20,
+    selectedPage = 15,
+    pages = []
+
+for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
+    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
+    
+    if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+        pages.push(currentPage);
+    }
+    
+}
+
+console.log(pages);
+
+/* PAGINAÇÃO
+CASO DE EXEMPLO 
+
+* totalPages = 20
+
+selectedPage = 6
+[1, ..., 4, 5, 6, 7, 8, ..., 20]
+
+ou
+
+selectedPage = 5
+[1, 2, 3, 4, 5, 6, 7, ..., 20]
+
+ou
+
+selectedPage = 15
+[1, ..., 13, 14, 15, 16, 17, ..., 20]
+
+*/
+
